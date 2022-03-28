4d589e697291d6d6f2fabe3ffbf591d42d3c25dc
shivanismane <97173103+shivanismane@users.noreply.github.com>
2022-03-28 10:46:46 -0700

    last commit

---
+++ app.js
@@ -0,0 +1,22 @@
+let mymsg = () =>
+ {
+    
+    let a = document.querySelector("#studnm").value;
+    document.querySelector("#snm").innerHTML = a;
+
+    let b = document.querySelector("#studcls").value;
+    document.querySelector("#scls").innerHTML = b;
+
+    let c = document.querySelector("#studroll").value;
+    document.querySelector("#srn").innerHTML = c;
+
+    let x = document.querySelector("#studmark").value;
+    document.querySelector("#smark").innerHTML = x;
+
+    let y = document.querySelector("#tnm").value;
+    document.querySelector("#tnm1").innerHTML = y;
+
+    let z = document.querySelector("#Mobileno").value;
+    document.querySelector("#mno").innerHTML = x;
+    
+};
